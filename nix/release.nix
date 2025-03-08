1b2b5b9432c9ba9cda98a004f74cc6173c3c9c88
Break Yang <breakds@gmail.com>
2025-03-08 12:58:28 -0800

    Fix release

--- nix/release.nix
+++ nix/release.nix
@@ -3,19 +3,19 @@
 let self = inputs.self;
 
 in {
+  flake.overlays.default = final: prev: {
+    rsu-taxer-webapp = final.callPackage ./pkgs/webapp.nix {};
+    pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+      (python-final: python-prev: {
+        rsu-taxer = python-final.callPackage ./pkgs/rsu-taxer.nix {};
+      })
+    ];
+  };
+
   perSystem = { system, pkgs, ... }: {
     _module.args.pkgs = import inputs.nixpkgs {
       inherit system;
-      overlays = [
-        (final: prev: {
-          rsu-taxer-webapp = final.callPackage ./pkgs/webapp.nix {};
-          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-            (python-final: python-prev: {
-              rsu-taxer = python-final.callPackage ./pkgs/rsu-taxer.nix {};
-            })
-          ];
-        })
-      ];
+      overlays = [ self.overlays.default ];
     };
 
     packages = {
